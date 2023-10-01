8477930d5bf8d79b8f636d39e9534a432cb11ebf
beebablaster <sanzharsyzdykov1738@gmail.com>
2023-10-02 02:16:56 +0600

    feat - validation for all input fields

--- app/modules/Core/components/RestoreForm/index.tsx
+++ app/modules/Core/components/RestoreForm/index.tsx
@@ -1,31 +1,90 @@
 'use client'
 
-import {Button, Card, CardBody, Flex, Heading, Input, Text} from "@chakra-ui/react";
+import {
+    Button,
+    Card,
+    CardBody,
+    Flex,
+    FormControl,
+    FormErrorMessage,
+    Heading,
+    Input,
+    InputGroup, InputRightElement,
+    Text
+} from "@chakra-ui/react";
 import {useState} from "react";
+import {useForm} from "react-hook-form";
 
 export default function RestoreForm() {
-    const [number, setNumber] = useState('')
-    const [password, setPassword] = useState('')
+    const [show, setShow] = useState(false)
 
-    const isPasswordFieldShown = true
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting },
+    } = useForm()
+
+    const onSubmit = (values) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+                resolve()
+            }, 3000)
+        })
+    }
+
     return (
         <Card>
             <CardBody>
                 <Flex direction='column' gap='32px'>
                     <Heading>Restore your password</Heading>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                     <Flex direction='column' gap='32px'>
-                        <Flex direction='column' gap='12px'>
+                        <Flex direction='column' gap='16px'>
                             <Flex direction='column' gap='4px'>
                                 <Text>Your number</Text>
-                                <Input onChange={(e) => setNumber(e.target.value)} placeholder='+7 (777) 777 77 77'/>
+                                <FormControl isInvalid={errors.number}>
+                                    <Input id='number'
+                                           placeholder='+7 (777) 777 77 77'
+                                           {...register('number', {
+                                               required: 'Phone number is required',
+                                               pattern: /\+\d\s\([0-9]+\)\s[0-9]+\s[0-9]+\s[0-9]+/i,
+                                               minLength: { value: 11, message: 'Number must be of the following format: +7 (777) 777 77 77' },
+                                               maxLength: { value: 18, message: 'Number must be of the following format: +7 (777) 777 77 77'},
+                                           })}
+                                    />
+                                    <FormErrorMessage>
+                                        {errors.number && errors.number.message}
+                                    </FormErrorMessage>
+                                </FormControl>
                             </Flex>
                             <Flex direction='column' gap='4px'>
                                 <Text>Enter a new password</Text>
-                                <Input onChange={(e) => setPassword(e.target.value)} placeholder='*******'/>
+                                <FormControl isInvalid={errors.password}>
+                                    <InputGroup>
+                                        <Input id='password'
+                                               placeholder='Enter password'
+                                               type={show ? 'text' : 'password'}
+                                               {...register('password', {
+                                                   required: 'Password is required',
+                                                   minLength: { value: 5, message: 'Your password must be at least 5 characters long' },
+                                               })}
+                                        />
+                                        <InputRightElement width='4.5rem'>
+                                            <Button size='xs' onClick={() => setShow(!show)}>
+                                                {show ? 'Hide' : 'Show'}
+                                            </Button>
+                                        </InputRightElement>
+                                    </InputGroup>
+                                    <FormErrorMessage>
+                                        {errors.password && errors.password.message}
+                                    </FormErrorMessage>
+                                </FormControl>
                             </Flex>
                         </Flex>
-                            <Button>Restore</Button>
+                            <Button type='submit' colorScheme='teal'>Restore</Button>
                     </Flex>
+                    </form>
                 </Flex>
             </CardBody>
         </Card>
