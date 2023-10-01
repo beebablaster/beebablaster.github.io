57c702399d367d8e964ee990e146bfab2d006b4e
Damir <56208369+beebablaster@users.noreply.github.com>
2023-10-02 03:38:34 +0600

    feat: form validation (#8)
    * feat - init validation form for phone number and password
    
    * feat - validation for all input fields
    
    * feat - register button conditional functionality, added react hook form to readme
    
    * feat - new validation regex, conditional rendering, error messages

--- app/modules/Core/components/AuthorizationForm/index.tsx
+++ app/modules/Core/components/AuthorizationForm/index.tsx
@@ -1,36 +1,111 @@
 'use client'
 
-import {Button, Card, CardBody, Flex, Heading, Input, Link, Text} from "@chakra-ui/react";
+import {
+    Button,
+    Card,
+    CardBody,
+    Flex,
+    FormControl,
+    FormErrorMessage,
+    Heading,
+    Input, InputGroup,
+    InputRightElement,
+    Link,
+    Text
+} from "@chakra-ui/react"
 import NextLink from 'next/link'
-import {useState} from "react";
+import { useForm, Controller } from "react-hook-form"
+import {useState} from "react"
 
 export default function AuthorizationForm() {
-    const [number, setNumber] = useState('')
-    const [password, setPassword] = useState('')
+    const [show, setShow] = useState(false)
+
+    const {
+        handleSubmit,
+        register,
+        control,
+        formState: { errors },
+    } = useForm({
+        mode: 'onChange'
+    })
+
+    const onSubmit = (values) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+                resolve()
+            }, 3000)
+        })
+    }
 
     return (
         <Card>
             <CardBody>
                <Flex direction='column' gap='32px'>
                <Heading>Sign in</Heading>
+                   <form onSubmit={handleSubmit(onSubmit)}>
                    <Flex direction='column' gap='32px'>
                        <Flex direction='column' gap='16px'>
                            <Flex direction='column' gap='4px'>
                                <Text>Your number</Text>
-                               <Input onChange={(e) => setNumber(e.target.value)} placeholder='+7 (777) 777 77 77'/>
+                               <FormControl isInvalid={errors.number}>
+                                   <Controller
+                                       name='number'
+                                       control={control}
+                                       defaultValue=""
+                                       rules={{
+                                           required: 'Phone number is required',
+                                           pattern: {
+                                               value: /\+\d\s\([0-9]+\)\s[0-9]+\s[0-9]+\s\d\d/i,
+                                               message: 'Invalid number format',
+                                           },
+                                           minLength: { value: 11, message: 'Number must be of the following format: +7 (777) 777 77 77' },
+                                           maxLength: { value: 18, message: 'Number must be of the following format: +7 (777) 777 77 77'},
+                                       }}
+                                       render={({ field }) => (
+                                           <Input {...field} placeholder='+7 (777) 777 77 77' />
+                                       )}/>
+                               <FormErrorMessage>
+                                   {errors.number && errors.number.message}
+                               </FormErrorMessage>
+                               </FormControl>
                            </Flex>
-                           <Flex direction='column' gap='4px'>
-                               <Text>Your password</Text>
-                               <Input onChange={(e) => setPassword(e.target.value)} placeholder='*******'/>
-                           </Flex>
+                           {!errors.number && (
+                               <Flex direction='column' gap='4px'>
+                                   <Text>Your password</Text>
+                                   <FormControl isInvalid={errors.password}>
+                                       <InputGroup>
+                                           <Input id='password'
+                                                  placeholder='Enter password'
+                                                  type={show ? 'text' : 'password'}
+                                                  {...register('password', {
+                                                      required: 'Password is required',
+                                                      minLength: { value: 5, message: 'Your password must be at least 5 characters long' },
+                                                  })}
+                                           />
+                                           <InputRightElement width='4.5rem'>
+                                               <Button size='xs' onClick={() => setShow(!show)}>
+                                                   {show ? 'Hide' : 'Show'}
+                                               </Button>
+                                           </InputRightElement>
+                                       </InputGroup>
+                                       <FormErrorMessage>
+                                           {errors.password && errors.password.message}
+                                       </FormErrorMessage>
+                                   </FormControl>
+                               </Flex>
+                           )}
                        </Flex>
+
                        <Flex justifyContent='space-between' gap='64px'>
                        <Link as={NextLink} href='/restore'>
                            Forgot your password?
                        </Link>
-                           <Button>Login</Button>
+                           {!errors.number ? <Button type='submit' colorScheme='teal'>Login</Button> :
+                               <Button type='submit' colorScheme='teal'>Register</Button>}
                        </Flex>
                    </Flex>
+                   </form>
                </Flex>
             </CardBody>
         </Card>
--- app/modules/Core/components/RegistrationForm/index.tsx
+++ app/modules/Core/components/RegistrationForm/index.tsx
@@ -1,28 +1,41 @@
 'use client'
 
-import {Button, Card, CardBody, Checkbox, Flex, Heading, Input, Text} from "@chakra-ui/react";
+import {
+    Button,
+    Card,
+    CardBody,
+    Checkbox,
+    Flex,
+    FormControl,
+    FormErrorMessage,
+    Heading,
+    Input, InputGroup, InputRightElement,
+    Text
+} from "@chakra-ui/react";
 import {useDispatch} from "react-redux";
 import {signUp} from "../../../../../redux/features/auth-slice";
 import {AppDispatch} from "../../../../../redux/store";
 import {useState} from "react";
+import {useForm} from "react-hook-form";
 
 export default function RegistrationForm() {
     const dispatch = useDispatch<AppDispatch>()
-    const [number, setNumber] = useState('')
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [show, setShow] = useState(false)
     const [checkboxIsClicked, setCheckboxIsClicked] = useState(false)
 
-    const onRegistrationFormSubmit = () => {
-        const payload = {
-            isAuth: true,
-            number: number,
-            password: password,
-            name: name,
-            email: email
-        }
-        dispatch(signUp(payload))
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isValid },
+    } = useForm()
+
+    const onSubmit = (values) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+                resolve()
+            }, 3000)
+        })
     }
 
     return (
@@ -30,32 +43,100 @@ export default function RegistrationForm() {
             <CardBody>
                 <Flex direction='column' gap='32px'>
                     <Heading>Sign up</Heading>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                     <Flex direction='column' gap='32px'>
-                        <Flex direction='column' gap='12px'>
+                        <Flex direction='column' gap='16px'>
                             <Flex direction='column' gap='4px'>
                                 <Text>Your number</Text>
-                                <Input onChange={(e) => setNumber(e.target.value)} placeholder='+7 (777) 777 77 77'/>
+                                <FormControl isInvalid={errors.number}>
+                                    <Input id='number'
+                                           placeholder='+7 (777) 777 77 77'
+                                           {...register('number', {
+                                               required: 'Phone number is required',
+                                               pattern: {
+                                                   value: /\+\d\s\([0-9]+\)\s[0-9]+\s[0-9]+\s\d\d/i,
+                                                   message: 'Invalid number format',
+                                               },
+                                               minLength: { value: 11, message: 'Number must be of the following format: +7 (777) 777 77 77' },
+                                               maxLength: { value: 18, message: 'Number must be of the following format: +7 (777) 777 77 77'},
+                                           })}
+                                    />
+                                    <FormErrorMessage>
+                                        {errors.number && errors.number.message}
+                                    </FormErrorMessage>
+                                </FormControl>
                             </Flex>
                             <Flex direction='column' gap='4px'>
                                 <Text>Your name</Text>
-                                <Input onChange={(e) => setName(e.target.value)} placeholder='Name'/>
+                                <FormControl isInvalid={errors.name}>
+                                    <Input id='name'
+                                           placeholder='Your name'
+                                           {...register('name', {
+                                               required: 'Name is required',
+                                               pattern: {
+                                                   value: /^[a-z ,.'-]+$/i,
+                                                   message: 'Invalid name format'
+                                               },
+                                               minLength: { value: 2, message: 'Name must be at least 2 characters long' },
+                                           })}
+                                    />
+                                    <FormErrorMessage>
+                                        {errors.name && errors.name.message}
+                                    </FormErrorMessage>
+                                </FormControl>
                             </Flex>
                             <Flex direction='column' gap='4px'>
                                 <Text>Your Email</Text>
-                                <Input onChange={(e) => setEmail(e.target.value)} placeholder='damirsyzdykov@gmail.com'/>
+                                <FormControl isInvalid={errors.email}>
+                                    <Input id='email'
+                                           placeholder='Your Email'
+                                           {...register('email', {
+                                               required: 'Email is required',
+                                               pattern: {
+                                                   value: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+                                                   message: 'Invalid Email format'
+                                               },
+                                               minLength: { value: 3, message: 'Email must be of the following format: hello@example.com' },
+                                           })}
+                                    />
+                                    <FormErrorMessage>
+                                        {errors.email && errors.email.message}
+                                    </FormErrorMessage>
+                                </FormControl>
                             </Flex>
                             <Flex direction='column' gap='4px'>
                                 <Text>Your password</Text>
-                                <Input onChange={(e) => setPassword(e.target.value)} placeholder='*******'/>
+                                <FormControl isInvalid={errors.password}>
+                                    <InputGroup>
+                                        <Input id='password'
+                                               placeholder='Enter password'
+                                               type={show ? 'text' : 'password'}
+                                               {...register('password', {
+                                                   required: 'Password is required',
+                                                   minLength: { value: 5, message: 'Your password must be at least 5 characters long' },
+                                               })}
+                                        />
+                                        <InputRightElement width='4.5rem'>
+                                            <Button size='xs' onClick={() => setShow(!show)}>
+                                                {show ? 'Hide' : 'Show'}
+                                            </Button>
+                                        </InputRightElement>
+                                    </InputGroup>
+                                    <FormErrorMessage>
+                                        {errors.password && errors.password.message}
+                                    </FormErrorMessage>
+                                </FormControl>
                             </Flex>
                         </Flex>
                         <Flex justifyContent='space-between' gap='64px'>
                             <Checkbox isChecked={checkboxIsClicked} whiteSpace='wrap' onChange={() => setCheckboxIsClicked(!checkboxIsClicked)}>
                                  I agree to having my  <br />personal data processed
                             </Checkbox>
-                            <Button onClick={onRegistrationFormSubmit}>Register</Button>
+                            {(isValid && checkboxIsClicked) ? <Button type='submit' colorScheme='teal'>Register</Button> :
+                                <Button type='submit' disabled={true}>Register</Button>}
                         </Flex>
                     </Flex>
+                    </form>
                 </Flex>
             </CardBody>
         </Card>
